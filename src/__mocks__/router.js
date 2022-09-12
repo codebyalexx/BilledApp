8ed5b346a90bd0547f5365ef0cdf1332fdc384cc
alexandrelg2022 <a.legal.contact@gmail.com>
2022-09-13 00:28:18 +0200

    :clown_face: Ajout du support pour NewBill sur le Router mocké

--- src/__mocks__/router.js
+++ src/__mocks__/router.js
@@ -12,5 +12,12 @@ export default () => {
     const divIcon2 = document.getElementById('layout-icon2')
     divIcon1.classList.add('active-icon')
     divIcon2.classList.remove('active-icon')
+  } else if (window.location.hash === ROUTES_PATH['NewBill']) {
+    rootDiv.innerHTML = ROUTES({ pathname: window.location.hash, loading: true })
+    /* It's highlighting the side icon */
+    const divIcon1 = document.getElementById('layout-icon1')
+    const divIcon2 = document.getElementById('layout-icon2')
+    divIcon1.classList.remove('active-icon')
+    divIcon2.classList.add('active-icon')
   }
 }
