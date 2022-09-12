cd3f74289c9ec0f0a8dea16c403846f9f88539f1
alexandrelg2022 <a.legal.contact@gmail.com>
2022-09-12 23:37:25 +0200

    :white_check_mark: Ajout test NewBill pour l'icône dans le layout vertical

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -4,15 +4,35 @@
 
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import Router from "../app/Router.js";
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js";
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then new bill icon in vertical layout should be highlighted", () => {
+      /* It's setting user type as Employee in localStorage */
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee"
+        })
+      )
+
+      /* It's rendering BillsUI in order to test icon */
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      const newBillUI = NewBillUI()
+      root.innerHTML = newBillUI
+      document.body.appendChild(root)
+
+      /* It's defining URL to NewBill */
+      window.location.assign(ROUTES_PATH['NewBill'])
+      Router()
+
+      /* It's testing if bills icon is active (highlighted) */
+      const billsIcon = screen.getByTestId("icon-mail")
+      expect(billsIcon.classList.contains("active-icon")).toBeTruthy()
     })
   })
 })
